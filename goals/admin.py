da4d0f0061639215277d0ff4e7417ad1382ee92b
RuthWachira <nyagaki.wachira@gmail.com>
2025-07-24 18:20:47 +0300

    1.Split models, updated model attributes and migrated 2.Registred models to admin

--- goals/admin.py
+++ goals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Goals, Analysis
+
 
 # Register your models here.
+admin.site.register(Goals)
+admin.site.register(Analysis)
--- goals/models.py
+++ goals/models.py
@@ -4,16 +4,36 @@ from django.db import models  # preimported by django
 
 
 class Goals(models.Model):
+    TIMEFRAME_CHOICES = [
+        ("long-term", "Long-term"),
+        ("short-term", "Short-term")
+    ]
+    STATUS_CHOICES = [
+        ("not started", "Not started"),
+        ("in progress", "In progress"),
+        ("completed", "Completed")
+    ]
     name = models.CharField(max_length=255)
-    # to update to dropdown later and hence adjust the field class.
-    priority = models.CharField(max_length=255)
-    # to update to dropdown later and hence adjust the field class.
-    timeline = models.CharField(max_length=255)
-    planned_completion_date = models.DateField(
-        auto_now_add=True)  # to update to a calendar picker widget
-    start_date = models.DateField(auto_now_add=True)
-      # to update to a calendar picker widget
-    progress_notes = models.CharField
-    actual_completion_date = models.DateField(
-        auto_now_add=True)  # to update to a calendar picker widget
-    lessons_learnt = models.CharField
+    date_created = models.DateField(auto_now_add=True)
+    description = models.TextField(default="No description provided")
+    priority = models.CharField(max_length=255)  # i.e urgent and important etc
+    # to update to dropdown later  with choice selection and hence adjust the field class.
+    timeframe = models.CharField(
+        choices=TIMEFRAME_CHOICES, max_length=255)
+    # i.e long-term or short-term
+    planned_completion_date = models.DateField(null=True, blank=True)
+    # to update to a calendar picker widget
+    start_date = models.DateField(null=True, blank=True)
+    # to update to a calendar picker widget
+    # to update to a calendar picker widget
+    completion_date = models.DateField(null=True, blank=True)
+    status = models.CharField(
+        choices=STATUS_CHOICES, max_length=255)
+
+
+class Analysis(models.Model):
+    goal = models.ForeignKey(Goals, on_delete=models.CASCADE)
+    date_created = models.DateField(auto_now_add=True)
+    challenges = models.TextField(default="No challenge indicated")
+    lessons = models.TextField(default="No lesson indicated")
+    proposed_action = models.TextField(default="No proposed action")
