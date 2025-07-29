ce8dbb29703cc8200b5fe6a88411100caeefb1cf
RuthWachira <nyagaki.wachira@gmail.com>
2025-07-29 16:34:31 +0300

    Added user field in Goals model

--- goals/models.py
+++ goals/models.py
@@ -1,4 +1,5 @@
 from django.db import models  # preimported by django
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -13,6 +14,7 @@ class Goals(models.Model):
         ("in progress", "In progress"),
         ("completed", "Completed")
     ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length=255)
     date_created = models.DateField(auto_now_add=True)
     description = models.TextField(default="No description provided")
