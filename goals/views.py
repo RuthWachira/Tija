b79688ab8aab14937e75b069a116d7b1dd3f844c
RuthWachira <nyagaki.wachira@gmail.com>
2025-08-04 17:48:10 +0300

    updated index view to display goal names

--- goals/views.py
+++ goals/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render  # preimported by django
 from django.http import HttpResponse  # manually imported by me
+from .models import Goal
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Howdy! howdy! howdy to you!. Welcome aboard and set a GOAL")
+    # return HttpResponse("Howdy! howdy! howdy to you!. Welcome aboard and set a GOAL")   #initial view before updating it to display goal names
+    goals = Goal.objects.all()   #error does not affect runtime, can be solved by installing pylint
+    goal_names = [x.name for x in goals]
+    return HttpResponse(','.join(goal_names))
