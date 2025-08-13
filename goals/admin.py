cf0532a39aa253b776e333f2d47351ee918a79fe
RuthWachira <nyagaki.wachira@gmail.com>
2025-08-13 18:19:55 +0300

    1.Updated admin to show readonly field(date_created) 2.Created index.html template and corresponding view function

--- goals/admin.py
+++ goals/admin.py
@@ -5,11 +5,13 @@ from .models import Goal, Review  # manually imported by me
 class GoalAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'user']
     search_fields = ['name']
+    readonly_fields = ['date_created']
 
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'goal']
     search_fields = ['goal']
+    readonly_fields = ['date_created']
 
 
 # Register your models here.
--- goals/views.py
+++ goals/views.py
@@ -6,6 +6,9 @@ from .models import Goal
 
 def index(request):
     # return HttpResponse("Howdy! howdy! howdy to you!. Welcome aboard and set a GOAL")   #initial view before updating it to display goal names
-    goals = Goal.objects.all()   #error does not affect runtime, can be solved by installing pylint
-    goal_names = [x.name for x in goals]
-    return HttpResponse(','.join(goal_names))
+    # pylint error on Goal.objects does not affect runtime, can be solved by installing pylint django.
+    goals = Goal.objects.all()
+    # goal_names = [x.name for x in goals]  #commented out to keep track, we incorporated initial html template rendering afterwards.
+    # return HttpResponse(','.join(goal_names))  #initial view before updating to initial html template rendering
+    # return render(request, 'index.html')  #initial rendering that only read the testing text typed on the index.html i.e Hello world
+    return render(request, 'index.html', {'goals': goals})
